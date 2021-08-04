f40661e9a1679470de5e93b8207ffd6a29c9347e
DEV <dev@info.local>
2021-08-04 16:46:48 -0300

    modelo arrumado dnv

---
+++ API/Projeto_Escola/Projeto_Escola/Interfaces/IDataMovimentacao.cs
@@ -0,0 +1,17 @@
+﻿using Projeto_Escola.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Escola.Interfaces
+{
+    interface IDataMovimentacao
+    {
+        List<DataMovimentacao> Listar();
+
+        void Cadastrar(DataMovimentacao novaMovimentacao);
+
+        
+    }
+}
---
+++ API/Projeto_Escola/Projeto_Escola/Repository/DataMovimentacao.cs
@@ -0,0 +1,15 @@
+﻿using Projeto_Escola.Contexts;
+using Projeto_Escola.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Escola.Repository
+{
+    public class DataMovimentacao : 
+    {
+        ProjetoContext ctx = new ProjetoContext();
+        
+    }
+}
